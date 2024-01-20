9cdfa8d6e2794a0a7316be44b60de6693c7585a0
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2024-01-19 23:20:00 -0300

    Update separador.py

--- Reutilizavel/separador.py
+++ Reutilizavel/separador.py
@@ -4,7 +4,21 @@
 #saida str{sep_i}ing {sep_if} str{sep_f}ing ex: 123.456,789
 #ex:    {123}{.}{456}{,}{789}
 def formatar_leitura(string="",iden=".",qua_i=3,sep_i=".",sep_if=",",qua_f=None,sep_f=None):
-    
+     """Formata uma string no formato desejado.
+
+    Args:
+        string (str): A string a ser formatada.
+        iden (str): O identificador que separa a parte inteira da parte fracionária (padrão é '.').
+        qua_i (int): A quantidade de caracteres na parte inteira antes de inserir o separador (padrão é 3).
+        sep_i (str): O separador a ser inserido na parte inteira (padrão é '.').
+        sep_if (str): O separador entre a parte inteira e a parte fracionária (padrão é ',').
+        qua_f (int): A quantidade de caracteres na parte fracionária antes de inserir o separador (padrão é None).
+        sep_f (str): O separador a ser inserido na parte fracionária (padrão é None).
+
+    Returns:
+        str: A string formatada.
+    """
+    # Separa a parte inteira da parte real
     #separa a parte inteira da real
     string=string.split(iden)
     

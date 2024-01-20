816c27bd88a7220ce1c747a45402b42f94c41553
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2024-01-19 23:21:26 -0300

    Update BaseDados.py

--- Reutilizavel/BaseDados.py
+++ Reutilizavel/BaseDados.py
@@ -1,150 +1,186 @@
 class BaseDados:
     """
     Esta base de dados depois de criada, sempre permite que adicionem mais dados a ela,
-    ela adiciona dados como se fosse uma nova linha, para isso ele permite apenas recebimento de novos dados
-    do tipo dict, caso esse dict recebido tenha uma chave não cadastrada nos dados, ele cria uma nova e atribui None
+    ela adiciona dados como se fosse uma nova linha, para isso ela permite apenas recebimento de novos dados
+    do tipo dict, caso esse dict recebido tenha uma chave não cadastrada nos dados, ela cria uma nova e atribui None
     aos valores para as linhas anteriores.
     """
-    def __init__(self,dados={}):
-        #cria base de dados vazia e pede para o setter receber os dados
-        self.__dados={}
-        self.__quantidade_linhas=0
-        self.__quantidade_colunas=0
-        self.__colunas=[]
+    def __init__(self, dados={}):
+        """
+        Inicializa a BaseDados.
+
+        Args:
+            dados (dict): Dados iniciais para a base de dados (padrão é {}).
+        """
+        self.__dados = {}
+        self.__quantidade_linhas = 0
+        self.__quantidade_colunas = 0
+        self.__colunas = []
         if dados != {}:
-            self.dados=dados
-    
-    #geters
-    
+            self.dados = dados
+
+    # Getters
+
     @property
     def colunas(self):
+        """Retorna as colunas da base de dados."""
         return self.__colunas
-        
+
     @property
     def linhas(self):
+        """Retorna a quantidade de linhas na base de dados."""
         return self.__quantidade_linhas
-    
+
     @property
     def dados(self):
+        """Retorna os dados da base."""
         return self.__dados
-    
+
     @property
     def shape(self):
-        return (self.__quantidade_colunas,self.__quantidade_linhas)
-        
-    #metodos
-    def remover_coluna(self,chave):
-        if self.dados.get(chave) != None:
+        """Retorna a forma (quantidade de colunas, quantidade de linhas) da base de dados."""
+        return (self.__quantidade_colunas, self.__quantidade_linhas)
+
+    # Métodos
+
+    def remover_coluna(self, chave):
+        """
+        Remove uma coluna da base de dados.
+
+        Args:
+            chave (str): A chave da coluna a ser removida.
+
+        Raises:
+            ValueError: Se a coluna não estiver cadastrada.
+        """
+        if self.dados.get(chave) is not None:
             self.__dados.pop(chave)
-            self.__quantidade_colunas-=1
+            self.__quantidade_colunas -= 1
         else:
-            raise ValueError(f"Coluna {i} não está cadastrada")
-            
-    def remover_linha(self,i):
+            raise ValueError(f"Coluna {chave} não está cadastrada")
+
+    def remover_linha(self, i):
+        """
+        Remove uma linha da base de dados.
+
+        Args:
+            i (int): O índice da linha a ser removida.
+
+        Raises:
+            ValueError: Se a linha não estiver cadastrada.
+        """
         if 0 <= i < self.__quantidade_linhas:
             for coluna in self.dados:
                 self.__dados[coluna].pop(i)
-            self.__quantidade_linhas-=1
+            self.__quantidade_linhas -= 1
         else:
             raise ValueError(f"Linha {i} não está cadastrada")
-    
-    
-    #metodos especiais
+
+    # Métodos Especiais
+
     def __len__(self):
+        """Retorna a quantidade de colunas na base de dados."""
         return self.__quantidade_colunas
-        
+
     def __iter__(self):
+        """Retorna um iterador para os itens da base de dados."""
         return iter(self.dados.items())
-        
-    #precisa ser arrumado para mescla de duas classes iguais 
-    def __add__(self,outro):
-        retorno=BaseDados(self.dados)
-        if type(outro) == dict :
-            outro=BaseDados(outro)
-            
+
+    def __add__(self, outro):
+        """
+        Adiciona duas bases de dados.
+
+        Args:
+            outro (BaseDados, dict): Outra base de dados ou um dicionário de dados.
+
+        Returns:
+            BaseDados: Uma nova base de dados resultante da adição.
+        """
+        retorno = BaseDados(self.dados)
+        if type(outro) == dict:
+            outro = BaseDados(outro)
+
         for i in range(outro.linhas):
-            temp={chave:outro.dados[chave][i] for chave in outro.colunas}
-            retorno.dados=temp
-           
+            temp = {chave: outro.dados[chave][i] for chave in outro.colunas}
+            retorno.dados = temp
+
         return retorno
-     
-    def __getitem__(self,indice):
-        retorno=[]
+
+    def __getitem__(self, indice):
+        """
+        Obtém um item da base de dados.
+
+        Args:
+            indice (int, str, list): O índice, a chave ou a lista de chaves a serem obtidas.
+
+        Returns:
+            list, dict: Uma lista de valores ou um dicionário de valores, dependendo do tipo do índice.
+        """
+        retorno = []
         if type(indice) == list:
             for i in indice:
-                if self.dados.get(i,'não existe') != 'não existe':
+                if self.dados.get(i, 'não existe') != 'não existe':
                     retorno.append(self.dados[i])
                 else:
                     raise ValueError(f"Chave {i} não está cadastrada")
         else:
             if type(indice) != int:
-                retorno=self.dados[indice]
+                retorno = self.dados[indice]
             else:
-                retorno={chave:self.dados[chave][indice] for chave in self.colunas}
+                retorno = {chave: self.dados[chave][indice] for chave in self.colunas}
         return retorno
-     
-    #setters   
+
+    # Setters
+
     @dados.setter
-    def dados(self,dados):
-        
-        #caso esteja mesclando duas bases de dados 
-        if isinstance(dados,BaseDados):
+    def dados(self, dados):
+        """
+        Define os dados da base de dados.
+
+        Args:
+            dados (BaseDados, dict): Os dados a serem definidos na base. Pode ser outra BaseDados ou um dicionário.
+
+        Raises:
+            ValueError: Se os dados não pertencerem ao tipo BaseDados ou dict.
+        """
+        if isinstance(dados, BaseDados):
             for i in range(outro.linhas):
-                #converte a base de dados em um dict, com um unico valor, adicionando uma linha por vez
-                temp={chave:outro.dados[chave][i] for chave in outro.colunas}
-                self.dados=temp
-            
-        #tratamento de erro
-        if (type(dados) == dict and dados != {}):
-            
-            #quantidade de linhas adicionadas de uma vez
-            linhas_adicionadas=1
-            
-            for chave,valor in dados.items():
-                
-                #verifica se a chave já está cadastrada
-                if (f'{chave}'.lower() in self.__colunas) == False:
-                    #cria a chave e atribui None para as linhas de dados anteriores
-                    self.__dados[f'{chave}'.lower()]=[None for i in range(self.__quantidade_linhas)]
-                    #cadastra a nova coluna
+                temp = {chave: outro.dados[chave][i] for chave in outro.colunas}
+                self.dados = temp
+
+        if type(dados) == dict and dados != {}:
+            linhas_adicionadas = 1
+
+            for chave, valor in dados.items():
+                if f'{chave}'.lower() not in self.__colunas:
+                    self.__dados[f'{chave}'.lower()] = [None for i in range(self.__quantidade_linhas)]
                     self.__colunas.append(f'{chave}'.lower())
-                    self.__quantidade_colunas+=1
-                  
-                #adiciona o valor unico caso não seja uma lista 
-                if type(valor) != list: 
+                    self.__quantidade_colunas += 1
+
+                if type(valor) != list:
                     self.__dados[f'{chave}'.lower()].append(valor)
-                 
-                #mescla a lista a lista de valores anteriores  
                 else:
-                    self.__dados[f'{chave}'.lower()]=self.__dados[f'{chave}'.lower()]+valor
+                    self.__dados[f'{chave}'.lower()] = self.__dados[f'{chave}'.lower()] + valor
                     if linhas_adicionadas < len(valor):
-                        #atualiza a quantidade de linhas adicionadas
-                        linhas_adicionadas=len(valor)
-                    
-                
-            #atualiza a quantidade de linhas 
-            self.__quantidade_linhas+=linhas_adicionadas
-            
-            #atribui None para as chaves que os dados carregado não possui
-            for chave,valor in self.__dados.items():
+                        linhas_adicionadas = len(valor)
+
+            self.__quantidade_linhas += linhas_adicionadas
+
+            for chave, valor in self.__dados.items():
                 if len(valor) != self.__quantidade_linhas:
                     self.__dados[chave].append(None)
-          
-           
-        #tratamento de erro        
         else:
-            m='Dict vazio' if dados == {} else f"{dados} Não pertence ao tipo Dict"
+            m = 'Dict vazio' if dados == {} else f"{dados} Não pertence ao tipo Dict"
             raise ValueError(m)
-            
-            
-if __name__=='__main__':
-    dados1=BaseDados({"nome":'Hilster','IDAde':21})
-    dados1.dados={'nome':'Jader','IDAde':20}
-    dados2=BaseDados({"Nome":"Carlos","idade":50})
-    dados3=dados1+dados2
+
+if __name__ == '__main__':
+    dados1 = BaseDados({"nome": 'Hilster', 'IDAde': 21})
+    dados1.dados = {'nome': 'Jader', 'IDAde': 20}
+    dados2 = BaseDados({"Nome": "Carlos", "idade": 50})
+    dados3 = dados1 + dados2
     print(dados3.linhas)
     print(dados3.dados)
-    print(dados3[['nome','idade']])
+    print(dados3[['nome', 'idade']])
     print(dados3[0])
     print(len(dados3))
     print(dados3.shape)

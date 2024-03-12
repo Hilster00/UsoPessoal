19039350f8d6762ddae2ee589f953e9ebd40965b
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2024-03-12 01:40:18 -0300

    Update ControleMemorias.py

--- Reutilizavel/ControleMemorias.py
+++ Reutilizavel/ControleMemorias.py
@@ -1,29 +1,37 @@
 
 class memoria:
     def __init__(self,tamanho=0,nome="Memoria Qualquer"):
-        self.__tamanho=tamanho
-        self.__espaco_livre=tamanho
-        self.__dados={}
-        self.__nome=f"{nome}"
-        
-    @property
-    def tamanho(self):
-        return self.__tamanho
-    @tamanho.setter
-    def tamanho(self,tamanho):
+        #tamanho da memoria é imutavel
         if type(tamanho) not in [int, float]:
             tamanho=0
         self.__tamanho=tamanho
+        
+        self.__espaco_livre=tamanho
+        self.__dados={}
+        self.__nome=str(nome)
+        self.__nome_original=self.nome
+        
     @property
     def nome(self):
         return self.__nome
         
     @property
+    def nome_original(self):
+        return self.__nome_original
+    
+    def renomear(self,novo_nome):
+        self.__nome=novo_nome
+        
+    @property
+    def tamanho(self):
+        return self.__tamanho
+        
+    @property
     def livre(self):
         return self.__espaco_livre
     
-    def __ocupar_memoria(self,valor_tamanho):
-        self.__espaco_livre-=valor_tamanho
+    def __ocupar_memoria(self,valor):
+        self.__espaco_livre-=valor
         
     def adicionar(self,item):
         if type(item) in [list, tuple]:
@@ -41,19 +49,20 @@ class memoria:
 
         
     def __str__(self):
-        retorno=f"{self.__nome}\n"
-        retorno+=f"Capacidade:{self.__tamanho}\n"
-        retorno+=f"Espaço Livre:{self.__tamanho}\n"
+        retorno="" if self.nome_original == self.nome else f"Nome original:{self.nome_original}\n"
+        retorno+=f"Nome:{self.nome}\n"
+        retorno+=f"Capacidade:{self.__tamanho}\nEspaço Livre:{self.livre}\n"
         for item,tamanho in self.__dados.items():
             retorno+=f"{item} ocupa {tamanho}\n"
         return retorno
         
     def item(self,nome):
-        retorno=f"{self.__nome}\n"
+        retorno="" if self.nome_original == self.nome else f"{self.nome_original}\n"
+        retorno+=f"{self.nome}\n"
         if self.__dados.get(nome)!=None:
             retorno+=f"{nome} ocupa {self.__dados[nome]}\n"
         else:
-            retorno+=f"NULL ocupa 00\n"
+            retorno+=f"NULL ocupa NULL\n"
         return retorno
         
     def item_tamanho(self,item):
@@ -68,45 +77,50 @@ class memoria:
         if self.__dados.get(chave)!=None:
             retorno+=f"Item {chave} ocupa {self.__dados[chave]}"
         else:
-            retorno+=f"NULL ocupa 00\n"
+            retorno+=f"NULL ocupa NULL\n"
         return retorno
         
         
 class Memorias:
     def __init__(self, **kwargs):
-        self.__nome="Teste do Hilster"
-        if kwargs.get('nome') != None:
-            self.__nome=f"{kwargs.get('nome')}"
+        self.__nome=kwargs.get('nome',"Teste do Hilster")
             
-        self.__nomes_memorias={}
+        self.__nomes_memorias={}#achar a memoria pelo nome
         self.__n_memorias=0
-        
+        self.__item_memoria={}#indicara em qual memoria o item esta
+        self.__lista_nomes=[]
         if kwargs.get('memorias')!=None:
-            for i,m in enumerate(kwargs["memorias"]):
-                nome_memoria=f'{m.get("nome",f"Memoria {i}")}'
+            for m in kwargs["memorias"]:
+                nome_memoria=str(m.get("nome",f"Memoria {self.__n_memorias}"))
                 capacidade=m.get('capacidade',0)
                 m=memoria(capacidade,nome_memoria)
+                
+                #renomeia a memoria, para que não tenham duas com o mesmo nome
+                i=0
+                while nome_memoria in self.__lista_nomes:
+                    i+=1
+                    nome_memoria=f"Memoria {i}"
+                #o nome renomeado não afeta o nome original
+                if nome_memoria != m.nome:
+                    m.renomear(nome_memoria)
+                    
+                self.__nomes_memorias[nome_memoria]=m#acessar a memoria pelo nome
+                self.__nomes_memorias[self.__n_memorias]=m#acessar memoria pelo indice
+                
+                #adiciona o nome na lista de nomes usados
+                self.__lista_nomes.append(nome_memoria)
                 self.__n_memorias+=1
-                self.__nomes_memorias[m.nome]=m#acessar a memoria pelo nome
-                self.__nomes_memorias[i]=m#acessar memoria pelo indice
-            
-            
-        self.__item_memoria={}#indicara em qual memoria o item esta
         
     def __str__(self):
-        retorno=""
-        if self.__nome!=None:
-            retorno+=f"{self.__nome}\n"
-        retorno+=f"Quantidade de Memorias: {self.__n_memorias}\n\nMemorias:\n"
+
+        retorno=f"{self.__nome}\nQuantidade de Memorias: {self.__n_memorias}\n\nMemorias:\n"
         for i in range(self.__n_memorias):
-            retorno+="_"*30
-            retorno+="\n"
-            retorno+=f"Nome:"+str(self.__nomes_memorias[i])
-            retorno+="\n"
+            retorno=f"{retorno}{'_'*30}\n"
+            retorno+=f"{str(self.__nomes_memorias[i])}\n"
         return retorno
         
         
-    def adicionar_item(self,item,memoria=None):
+    def adicionar_item(self,item,m=None):
         if self.__item_memoria.get(item[0]) != None:
             return
         
@@ -114,18 +128,18 @@ class Memorias:
             if len(item) == 2:
                 if type(item[0]) == str and type(item[1]) in [int, float]:
                     
-                    if memoria!= None:
-                        memoria=self.__nomes_memorias[memoria]
-                        if memoria.livre>=item[1]:
-                            memoria.adicionar(item)
-                            self.__item_memoria[item[0]]=memoria
+                    if m != None:
+                        m=self.__nomes_memorias[m]
+                        if m.livre>=item[1]:
+                            m.adicionar(item)
+                            self.__item_memoria[item[0]]=m
                             return
                             
                     for i in range(self.__n_memorias):
                         if self.__nomes_memorias[i].livre>=item[1]:
-                            memoria=self.__nomes_memorias[i]
-                            memoria.adicionar(item)
-                            self.__item_memoria[item[0]]=memoria
+                            m=self.__nomes_memorias[i]
+                            m.adicionar(item)
+                            self.__item_memoria[item[0]]=m
                             break
                             
 
@@ -135,27 +149,43 @@ class Memorias:
     def adicionar(self, **memorias):
         if memorias.get('memorias')!=None:
             for m in memorias["memorias"]:
-                self.__n_memorias+=1
-                nome_memoria=f'{m.get("nome",f"Memoria {self.__n_memorias}")}'
+                
+                #dados da nova memoria inserida
+                nome_memoria=str(m.get("nome",f"Memoria {self.__n_memorias}"))
                 capacidade=m.get('capacidade',0)
                 m=memoria(capacidade,nome_memoria)
-                self.__nomes_memorias[m.nome]=m#acessar a memoria pelo nome
+                
+                #'renomeando' a memoria caso haja uma com o mesmo nome.
+                i=0
+                while nome_memoria in self.__lista_nomes:
+                    i+=1
+                    nome_memoria=f"Memoria {self.__n_memorias+i}"
+                    
+                #renomeando a memoria, o nome original permanece igual
+                if nome_memoria != m.nome:
+                    m.renomear(nome_memoria)    
+                    
+                #adicionando acesso a memoria
                 self.__nomes_memorias[self.__n_memorias]=m#acessar memoria pelo indice
+                self.__nomes_memorias[nome_memoria]=m#acessar a memoria pelo nome
+                
+                #adiciona o nome na lista de nomes usados
+                self.__lista_nomes.append(nome_memoria)
+                self.__n_memorias+=1
+                
             
     def remover_item(self,item):
         if self.__item_memoria.get(item) != None:
-            memoria=self.__item_memoria.get(item)
+            m=self.__item_memoria[item]
             memoria.remover(item)
             self.__item_memoria.pop(item)
             
     def item(self,item):
         if self.__item_memoria.get(item) != None:
-            memoria=self.__item_memoria.get(item)
-            retorno=""
-            if self.__nome!=None:
-                retorno+=f"{self.__nome}\n"
-            retorno+=str(memoria)
+            retorno=f"{str(self.__nome)}\n"
+            retorno+=str(self.__item_memoria[item])
             return retorno
+            
     def __getitem__(self, chave):
         if self.__nomes_memorias.get(chave)!=None:
             return self.__nomes_memorias[chave]
@@ -163,7 +193,7 @@ class Memorias:
     def limpar(self):
         self.__item_memoria={}
         for i in range(self.__n_memorias):
-            self.__nomes_memorias[i].limpar
+            self.__nomes_memorias[i].limpar()
 
     def reorganizar(self):
         itens=[]
@@ -171,7 +201,7 @@ class Memorias:
             item=[item,m.item_tamanho(item)]
             itens.append(item)
         itens=sorted(itens,key=lambda i: i[1])
-        self.limpar
+        self.limpar()
         for item in itens:
             self.adicionar_item(item)
             
@@ -185,3 +215,14 @@ if __name__=="__main__":
     print(teste)
     print(teste["Micro SD"])
     print(teste["Micro SD"][0])
+    teste.limpar()
+    print(teste)
+    teste.adicionar(memorias=[{'nome':'Micro SD',"capacidade":256}])
+    print(teste)
+    teste.adicionar_item(["Jogo 2",8])
+    print(teste)
+    teste.remover_item("Jogo 1")
+    print("JOGO REMOVIDO")
+    print(teste)
+    teste.adicionar_item(["Jogo 3",16],2)
+    print(teste.item("Jogo 3"))

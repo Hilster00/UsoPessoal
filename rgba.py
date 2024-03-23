e058a90546189091b0eb6b4905ab5b9116730c18
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2024-03-23 01:51:40 -0300

    Update rgba.py

--- rgba.py
+++ rgba.py
@@ -1,38 +1,35 @@
-lista=[str(i) for i in range(10)]
-lista+=[chr(i) for i in range(ord("A"),ord("G"))]
-
 def rgba(*args):
-    
-    #vazio é invalido
-    if len(args)<1:
+    lista = [str(i) for i in range(10)]
+    lista += [chr(i) for i in range(ord("A"), ord("G"))]
+
+    #aceita apenas 1, 3 ou 4 entradas
+    if len(args) not in [1,3,4]:
         return None
-         
-    #tratamento para entrada hexadecimal
     
-    if len(args) == 1 and type(args[0]) == str:
+    #verifica se os tipoes de entrada eram os esperados
+    if len(args) == 1 and not(isinstance(args[0], (list, tuple, dict,str))):
+        return None
+
+    #Tratamento para entrada hexadecimal
+    elif len(args) == 1 and isinstance(args[0], str):
         args=args[0]
+        
         #verifica se o tamanho está correto
-        if len(args)%3!=0:
-            if not((len(args)-1)%3!=0 and args[0]=="#"):
+        if len(args) % 3 != 0:
+            if not(((len(args)-1) % 3 != 0) and (args[0] == "#")):
                 return None
             args=args[1:]
         
         args=args.upper()
         
-               
         #verifica se todos os caracteres correspondem aos esperados
         for i in args:
             if i not in lista:
                 return None
-        
            
-        #verifica se o tamanho esta comprimido
+        #descompressão da string
         if len(args) == 3:
-            temp=f"{args}"
-            args=""
-            for i in range(3):
-                args+=f"{temp[i]}"*2
-        
+            args="".join([i*2 for i in args])
         
         """
         rgb=[]
@@ -41,53 +38,60 @@ def rgba(*args):
             valor+=lista.index(args[i+1])
             rgb.append(valor)
         """   
-        #cimplificação e otimização da função acima
+        #simplificação e otimização da função acima
         rgb=[(lista.index(args[i])*16)+lista.index(args[i+1]) for i in range(0,6,2)]    
         return rgb
 
-    elif not(len(args)==1 and type(args[0]) in [list,tuple,dict]):
-        if not(3<=len(args)<=4):
-            return None
-        if not(all([type(i)==int for i in args])):
-            return None
+    
+    elif len(args) == 1 and type(args[0]) == dict:
         
-        
-    if type(args) == dict:
-        temp=[]
-        for r in ["r",'R',"red","Red","RED"]:
-            if args.get(r) != None:
-                temp.append(args[r])
-                break
-        else:
-            return None
-        for g in ["g",'G',"green","Green","GREEN"]:
-            if args.get(g) != None:
-                temp.append(args[g])
-                break
-        else:
-            return None 
+        args=args[0]
+        #retira os valores de cada cor
+        verificar={'r':None,'g':None,'b':None,"a":None}
+        for v in args:
+            vv=v.upper()
+            if vv in ['R',"RED"]:
+                verificar['r']=args[v]
+                continue
+            if vv in ['G',"GREEN"]:
+                verificar['g']=args[v]
+                continue
+            if vv in ['B',"BLUE"]:
+                verificar['b']=args[v]
+                continue
+            if vv in ["A","ALPHA"]:
+                verificar['a']=args[v]
+                continue
             
-        for b in ["b",'B',"blue","Blue","BLUE"]:
-            if args.get(b) != None:
-                temp.append(args[b])
-                break
-        else:
+        #vetor que armazenara os valores
+        temp=[verificar[v] for v in 'rgba']
+        if not(all([i!=None for i in temp[:3]])):
             return None
-        for a in ["a",'A',"alpha","Alpha","ALPHA"]:
-            if args.get(b) != None:
-                temp.append(args[b])
-                break
-        args=temp
-    if all([0<=i<=255 for i in args])
-        char=""
-        for i in args[:3]:
-            char+=f"{lista[i//16]}{lista[i%16]}"
-        return char
-    
-    return None
-
-
-    
+        args = temp if temp[3]!=None else temp[:3]
         
-print(rgba(255,0,170))
-print(rgba("f0a"))
+    elif len(args) == 1 and isinstance(args[0], (list, tuple)):
+        args = args[0] if 3<=len(args[0])<=4 else [None]
+   
+    if not all([type(i) == int for i in args]):
+        return None
+
+    if not(all([0<=i<=255 for i in args])):
+        return None
+        
+    char="".join([f"{lista[i//16]}{lista[i%16]}" for i in args[:3]])
+    temp="".join([char[i*2] for i in range(3) if char[i*2]==char[1+i*2]])
+    return "#"+(char if len(temp) != 3 else temp)
+    
+    
+   
+
+
+if __name__ == "__main__":
+    print(rgba(170, 187, 204))
+    print(rgba([170, 187, 204]))
+    print(rgba((255, 0, 0)))
+    print(rgba((1, 1, 1)))
+    print(rgba((129, 129, 129)))
+    print(rgba((136, 136, 136)))
+    print(rgba("abc"))
+    print(rgba({'RED': 170, "g": 187, "BlUe": 204}))

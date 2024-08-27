7369117ef8185ff60791f2cbe00ded176dc092cb
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2024-08-27 03:40:12 -0300

    Update CalculadoraIP.py

--- Reutilizavel/CalculadoraIP.py
+++ Reutilizavel/CalculadoraIP.py
@@ -19,14 +19,18 @@ def converterD(n:str):
 ip=[int(i) for i in input("Digite o ip:").split(".")]
 mascara=int(input('Digite a mascara de rede:'))
 
+
+
 #temp para calcular ip da rede
 temp=mascara
 ip_rede=[]
 ip_broadcast=[]
 
+
 print()
 print("_"*50)
 print()
+
 
 #print do Ip da rede e calculo do ip da rede
 print('Ip da rede:',end="")
@@ -58,23 +62,3 @@ for v,i in enumerate(ip_rede):
         print(".",end="")
     else:
         print() 
-
-print()
-print("_"*50)
-print()
-
-print('Ip da rede em binario:',end="")
-for v,i in enumerate(ip_rede):
-    print(i.replace('-','0'),end="")
-    if v != 3:
-        print(".",end="")
-    else:
-        print()
-
-print('Ip do broadcast em binario:',end="")
-for v,i in enumerate(ip_broadcast):
-    print(converterB(i),end="")
-    if v != 3:
-        print(".",end="")
-    else:
-        print()
